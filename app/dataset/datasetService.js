3344459f2376c10534b21616250b84ee5f513f93
Jamie He <sijin.h@gmail.com>
2016-02-07 04:18:07 +0000

    dataset added (imcomplete)

---
+++ app/dataset/datasetService.js
@@ -0,0 +1,16 @@
+/**
+ * Created by iemam on 15/09/2015.
+ */
+'use strict';
+
+function datasetService($http, ngAppConfig){
+
+
+}
+
+angular
+    .module('eTRIKSdata.export')
+    .factory('datasetService',['$http', 'ngAppConfig', datasetService])
+
+
+
