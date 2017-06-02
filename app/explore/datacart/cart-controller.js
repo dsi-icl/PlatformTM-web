b60bdebb043f5464790d259bd2613cb3675d4c99
Ibrahim Emam <i.emam@imperial.ac.uk>
2017-06-02 16:19:48 +0100

    XFlinker fix for propagating filters

--- app/explore/datacart/cart-controller.js
+++ app/explore/datacart/cart-controller.js
@@ -1,29 +1,9 @@
 
 'use strict'
 function cartController($state,$stateParams,explorerService) {
-    //scope.add filters from exportService
-    //$scope.cart = exportService.getCart();
-
     var vm = this;
     vm.projectId = $stateParams.projectId;
     vm.queryId = $stateParams.queryId;
-
-   /* $scope.$watch('vm.cartservice.clickclack()',function (newval) {
-        //console.log(newval)
-        vm.cartQuery = cartService.getCurrentCartQuery();
-    },true)*/
-
-
-
-
-
-    vm.loadQuery = function(queryId){
-        $state.go('explore',{
-            projectId: vm.projectId,
-            queryId: queryId});
-    }
-
-
 }
 
 angular.module('biospeak.explorer')
