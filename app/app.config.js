610f4414512103e1f01b730fd896250dd30d653e
Ibrahim Emam <ibrahim.emam@gmail.com>
2022-09-28 14:50:14 +0100

    unicron updates

--- app/app.config.js
+++ app/app.config.js
@@ -63,6 +63,6 @@ biospeakApp.constant('USER_ROLES', {
 })
 
 biospeakApp.constant('ngAppConfig', {
-    //apiServiceBaseUri: '/api/v2/'
-    apiServiceBaseUri: 'http://localhost:5000/'
+    apiServiceBaseUri: '/api/v1/'
+    //apiServiceBaseUri: 'http://localhost:5000/'
 });
